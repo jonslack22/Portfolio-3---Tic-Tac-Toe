83b57c6ead8a21705051724c6263fcacc2c1b325
jonslack22 <jonathanslack89@ymail.com>
2022-12-09 11:19:06 +0000

    Initial commit with welcome message and fix display error of ASCii art

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+print("""\
+    \u001b[32m
+   _____ _        _____            _____          
+  |_   _(_)      |_   _|          |_   _|         
+    | |  _  ___    | | __ _  ___    | | ___   ___ 
+    | | | |/ __|   | |/ _` |/ __|   | |/ _ \ / _ |
+    | | | | (__    | | (_| | (__    | | (_) |  __/
+    \_/ |_|\___|   \_/\__,_|\___|   \_/\___/ \___|
+\u001b[0m
+""")
+
+
+print("Welcome to Jonathan Slack's Tic Tac Toe!\n")
+print("RULES: Using a format of a human versus computer, take turns "
+      "marking a space in a 3*3 grid. The player who succeeds in placing "
+      "three marks, represented by X and 0, in a horizontal, "
+      "vertical, or diagonal row, wins.\n")
+input("Press enter to continue.\n")
