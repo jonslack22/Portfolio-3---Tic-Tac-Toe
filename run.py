7de28ff09b110ca535cd4ba4d369f0c6732ec3be
jonslack22 <jonathanslack89@ymail.com>
2022-12-09 11:46:52 +0000

    Function for assigning X or O to player and computer in place

--- run.py
+++ run.py
@@ -17,27 +17,47 @@ def create_grid(grid):
     print('   |   |')
     print(' ' + grid[1] + ' | ' + grid[2] + ' | ' + grid[3])
     print('   |   |')
-print(create_grid())
-
-# print(
-#     """
-#     \u001b[32m
-#    _____ _        _____            _____          
-#   |_   _(_)      |_   _|          |_   _|         
-#     | |  _  ___    | | __ _  ___    | | ___   ___ 
-#     | | | |/ __|   | |/ _` |/ __|   | |/ _ \ / _ |
-#     | | | | (__    | | (_| | (__    | | (_) |  __/
-#     \_/ |_|\___|   \_/\__,_|\___|   \_/\___/ \___|
-# \u001b[0m
-# """)
 
 
-# print("Welcome to Jonathan Slack's Tic Tac Toe!\n")
-# print("RULES: Using a format of a human versus computer, take turns "
-#       "marking a space in a 3*3 grid. The player who succeeds in placing "
-#       "three marks, represented by X and 0, in a horizontal, "
-#       "vertical, or diagonal row, wins.\n")
-# input("Press enter to continue.\n")
+def choose_letter():
+    """
+    This function lets the player choose which letter they want to be, 
+    returning a list with the player’s letter as the first item, and the 
+    computer's letter as the second.
+    
+    The first element in the list is the player’s letter, the second is the 
+    computer's letter.
+    """
+    letter = ''
+    while not (letter == 'X' or letter == 'O'):
+        print('Do you want to be X or O?')
+        letter = input().upper()
+
+    if letter == 'X':
+        return ['X', 'O']
+    else:
+        return ['O', 'X']
+
+
+print(
+    """
+    \u001b[32m
+   _____ _        _____            _____          
+  |_   _(_)      |_   _|          |_   _|         
+    | |  _  ___    | | __ _  ___    | | ___   ___ 
+    | | | |/ __|   | |/ _` |/ __|   | |/ _ \ / _ |
+    | | | | (__    | | (_| | (__    | | (_) |  __/
+    \_/ |_|\___|   \_/\__,_|\___|   \_/\___/ \___|
+\u001b[0m
+""")
+
+
+print("Welcome to Jonathan Slack's Tic Tac Toe!\n")
+print("RULES: Using a format of a human versus computer, take turns "
+      "marking a space in a 3*3 grid. The player who succeeds in placing "
+      "three marks, represented by X and 0, in a horizontal, "
+      "vertical, or diagonal row, wins.\n")
+input("Press enter to continue.\n")
 
 while True:
     """
